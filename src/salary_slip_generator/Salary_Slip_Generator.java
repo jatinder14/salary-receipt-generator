a2b0581d7386e38817ad3100dd6c136aa438cb2f
jatinder14 <jatinder1901243@gmail.com>
2023-10-02 16:37:24 +0530

    prototype ready

---
+++ src/salary_slip_generator/Salary_Slip_Generator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package salary_slip_generator;
+
+/**
+ *
+ * @author dell
+ */
+public class Salary_Slip_Generator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
